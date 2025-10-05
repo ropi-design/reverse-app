9f7e24d50e782020ccf9b1247759b5c438e7226f
ropi-design <h.sato.tokyozidousya@gmail.com>
2025-10-05 09:29:42 +0900

    reverseできた

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Livewire\Volt\Volt;
 
 Route::get('/', function () {
     return view('welcome');
 });
+Volt::route('/reverse/{reverse}', 'reverse');
